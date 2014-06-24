ce22a2cdf35f1984878606547b4b13009069cf90
Ramesh <ramesh@wilshireaxon.com>
2014-06-24 12:08:17 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,39 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Aravind Vadali on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+{
+    CCNode *_catapultArm;
+    CCPhysicsNode *_physicsNode;
+}
+
+-(void)didLoadFromCCb
+{
+    self.userInteractionEnabled = TRUE;
+}
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [self launchPenguin];
+}
+
+-(void)launchPenguin
+{
+    CCNode *penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(15, 50));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
